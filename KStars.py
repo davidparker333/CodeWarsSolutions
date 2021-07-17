10470cfc55a1cc1157f58fbecc5c4792a9462806
David Parker <davidparker333@gmail.com>
2021-07-17 12:20:38 -0400

    cleaned up stars, added comments

--- KStars.py
+++ KStars.py
@@ -17,19 +17,35 @@ stars = [Star(i*100000, i) for i in range(1000)]
 
 def find_stars(star_list, k):
 
-    i = 0
+    # Initialize a result list
     result = []
 
+    # Check to see if there's enough stars to give back k as a result
     if len(star_list) < k:
         return -1
 
-    while i < k:
+    # Begin looping k number of times
+    for i in range(k):
+
+        # If the result list is full, quit the loop early and return result (stops us from having to loop through once we already have the result)
+        if len(result) == k:
+            return result
+
+        # Set the smallest to the largest value in the list to start
         smallest = max(star_list, key = lambda x: x.dist_from_earth)
+
+        # Begin looping through stars in the star list
         for star in star_list:
+
+            # Check if the star has a smaller distance than the current smallest, and whether or not its in the result list already
             if star not in result and star.dist_from_earth < smallest.dist_from_earth:
+
+                # If the star has a smaller distance and not in the result, make it the new smallest
                 smallest = star
+
+        # At the end of the loop, append the smallest star to the result
         result.append(smallest)
-        i+=1
+
     return result
 
 
