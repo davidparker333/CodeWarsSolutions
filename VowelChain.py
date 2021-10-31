00a95402a830c1cb766ca22fbb912375ab197d13
David Parker <davidparker333@gmail.com>
2021-10-31 12:19:13 -0400

    vowel chain

---
+++ VowelChain.py
@@ -0,0 +1,13 @@
+# The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that has alphabetic 
+# characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring. Vowels are any of aeiou.
+
+# Good luck!
+
+test = "ultrarevolutionariees"
+
+import re
+
+def solve(s):
+    return max([len(x) for x in re.findall("[aeiou]+", s)])
+
+print(solve(test))
