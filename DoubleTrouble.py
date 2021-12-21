81b232318e0a547e3b4ecf61ff65d863c7940c17
David Parker <davidparker333@gmail.com>
2021-12-21 16:57:53 -0500

    double trouble

---
+++ DoubleTrouble.py
@@ -0,0 +1,24 @@
+# Given an array of integers (x), and a target (t), you must find out if any two consecutive numbers in the array sum to t. If so, remove the second number.
+
+# Example:
+
+# x = [1, 2, 3, 4, 5]
+# t = 3
+
+# 1+2 = t, so remove 2. No other pairs = t, so rest of array remains:
+
+# [1, 3, 4, 5]
+
+# Work through the array from left to right.
+
+# Return the resulting array.
+
+def trouble(x, t):
+    lst = [x[0]]
+    for num in x[1:]:
+        if num + lst[-1] != t:
+            lst.append(num)
+    return lst
+
+
+print(trouble([1, 2, 3, 4, 5], 3))
