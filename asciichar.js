b34ecef7279db5881f11f599cea4d5a1212e7c85
David Parker <davidparker333@gmail.com>
2022-01-17 17:16:01 -0500

    ascii char value

---
+++ asciichar.js
@@ -0,0 +1,12 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+// should return:
+
+// 'A'
+
+getChar = (c) => String.fromCharCode(c);
+
+console.log(getChar(57));
