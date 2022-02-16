32aad0cbd712dc0ad008c5c4051b913ec345de25
David Parker <davidparker333@gmail.com>
2022-02-16 17:24:58 -0500

    digitize

---
+++ Digitize.py
@@ -0,0 +1,15 @@
+# Given a non-negative integer, return an array / a list of the individual digits in order.
+
+# Examples:
+
+# 123 => [1,2,3]
+
+# 1 => [1]
+
+# 8675309 => [8,6,7,5,3,0,9]
+
+
+def digitize(n): return [int(x) for x in str(n)]
+
+
+print(digitize(8675309))
