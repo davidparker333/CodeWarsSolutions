81116fd76bfa46f9217c573a91672bc83669cf9e
David Parker <davidparker333@gmail.com>
2022-02-24 17:23:41 -0500

    very even numbers

---
+++ VeryEven.py
@@ -0,0 +1,28 @@
+# Write a function that returns true if the number is a "Very Even" number.
+
+# If a number is a single digit, then it is simply "Very Even" if it itself is even.
+
+# If it has 2 or more digits, it is "Very Even" if the sum of its digits is "Very Even".
+
+# Examples
+# number = 88 => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd
+
+# number = 222 => returns true -> 2 + 2 + 2 = 6 => 6 is even
+
+# number = 5 => returns false
+
+# number = 841 => returns true -> 8 + 4 + 1 = 13 -> 1 + 3 => 4 is even
+# Note: The numbers will always be 0 or positive integers!
+
+
+def is_very_even_number(n):
+    return reduce(n) % 2 == 0
+
+
+def reduce(n):
+    while len(str(n)) > 1:
+        n = sum([int(x) for x in str(n)])
+    return n
+
+
+print(is_very_even_number(88))
