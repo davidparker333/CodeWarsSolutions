c83f073aea3cb066f4e2acbd657c50494a9425f3
David Parker <davidparker333@gmail.com>
2022-03-29 17:17:27 -0400

    numerical palindrome

---
+++ NumericalPalindrome.py
@@ -0,0 +1,17 @@
+# A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward. Examples of numerical palindromes are:
+
+# 2332
+# 110011
+# 54322345
+
+# For a given number num, write a function to test if it's a numerical palindrome or not and return a boolean (true if it is and false if not).
+
+# Return "Not valid" if the input is not an integer or less than 0.
+
+def palindrome(num):
+    if not isinstance(num, int) or num < 0:
+        return 'Not Valid'
+    return int(str(num)[::-1]) == num
+
+
+print(palindrome(2332))
