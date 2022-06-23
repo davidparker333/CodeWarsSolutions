f30808d191963beae98ca87ee5a3279687dbfef9
David Parker <davidparker333@gmail.com>
2022-06-23 17:12:18 -0400

    transpose strings

---
+++ TransposeStrings.py
@@ -0,0 +1,27 @@
+# You will be given an array that contains two strings. Your job is to create a function that will take those two strings and transpose them,
+# so that the strings go from top to bottom instead of left to right.
+
+# e.g. transposeTwoStrings(['Hello','World']);
+
+# should return
+
+# H W
+# e o
+# l r
+# l l
+# o d
+# A few things to note:
+
+# There should be one space in between the two characters
+# You don't have to modify the case (i.e. no need to change to upper or lower)
+# If one string is longer than the other, there should be a space where the character would be
+
+from itertools import zip_longest
+
+
+def transpose_two_strings(arr):
+    zipped = zip_longest(arr[0], arr[1])
+    return '\n'.join(['{0} {1}'.format(x if x else ' ', y if y else ' ') for x, y in zipped])
+
+
+print(transpose_two_strings(['Hello!', 'World']))
