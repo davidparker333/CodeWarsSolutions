ce5f540211ce49bcbd88b3b7d88dd47b175b885c
David Parker <davidparker333@gmail.com>
2022-08-03 17:36:35 -0400

    negative zero

---
+++ NegativeZero.py
@@ -0,0 +1,16 @@
+# There exist two zeroes: +0 (or just 0) and -0.
+
+# Write a function that returns true if the input number is -0 and false otherwise (True and False for Python).
+
+# In JavaScript / TypeScript / Coffeescript the input will be a number.
+
+# In Python / Java / C / NASM / Haskell / the input will be a float.
+
+import math
+
+
+def is_negative_zero(n):
+    return math.copysign(1, n) < 0 and n == 0
+
+
+print(is_negative_zero(-0.0))
